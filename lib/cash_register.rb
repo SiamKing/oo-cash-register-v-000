e01ee02b4181e52963029f16cb86bf40a9f2e035
SiamKing <thesiameseelectric@gmail.com>
2016-03-18 14:15:25 -0700

    cashed n carried

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,35 @@
 
+require 'pry'
+class CashRegister
+  attr_accessor :total, :discount, :item, :last_price
+
+
+  def initialize(discount = nil)
+    @total = 0
+    self.discount = discount
+    @items = []
+  end
+
+  def add_item(item, price, quantity = 1)
+    self.total += price * quantity
+    @last_price = total
+    (1..quantity).each {|i| @items << item}
+  end
+
+  def apply_discount
+    return "There is no discount to apply." if self.discount == nil
+    self.total = self.total - (self.total * (self.discount * 0.01)).floor
+    return "After the discount, the total comes to $#{total}."
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    @items.pop
+    self.total -= last_price
+    total
+  end
+
+end
